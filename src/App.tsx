265094b2b11fa3bf9fc579780e94ae7239d0581d
Hugodelima <hugorodrigues.contato@gmail.com>
2024-10-25 22:31:55 -0400

    fix: added the button handleCreate

--- src/App.tsx
+++ src/App.tsx
@@ -151,7 +151,9 @@ function App() {
         placeholder="Digite o saldo"
       />
 
-      
+      <button onClick={handleRegister}>
+        {editMode.enabled ? 'Alterar Conta' : 'Cadastrar Conta'}
+      </button>
 
       <hr />
       <Button onClick={handleOpen}>Open modal</Button>
